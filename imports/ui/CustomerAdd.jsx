5e503777a64e682a94af2e45c0d1d7cabf8c9272
Daniel <daniel.bainton@edu.omnia.fi>
2019-10-28 13:13:40 +0200

    updates to CustomerAdd

--- imports/ui/CustomerAdd.jsx
+++ imports/ui/CustomerAdd.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+function addCustomer() {
+	console.log("add customer");
+}
 
 class CustomerAdd extends Component {
 	render() {
 		return (
-			<form noValidate autoComplete="off">
-				<TextField id="customer-name"
-					label="Customer Name"
-				/>
-			</form>
+			<div>
+			<TextField id="customer-name"
+				label="Customer Name"
+				onKeyPress={(event) => {
+					if (event.key === "Enter") {
+						event.preventDefault();
+						addCustomer();
+					}
+				}}
+			/>
+			<br />
+			<Button variant="contained" color="primary"
+					onClick={(event) => { addCustomer(); }}>
+				Add
+			</Button>
+			</div>
 		);
 	}
 }
