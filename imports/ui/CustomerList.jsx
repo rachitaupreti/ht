17dcb6343b83203e60c08b6afd7f75d86a5dbc67
Daniel <daniel.bainton@edu.omnia.fi>
2019-10-28 12:01:44 +0200

    Change whitespace

--- imports/ui/CustomerList.jsx
+++ imports/ui/CustomerList.jsx
@@ -6,30 +6,30 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
 class Info extends Component {
-  render() {
-    const customers = this.props.customers.map(
-      customer => this.makeLink(customer)
-    );
+	render() {
+		const customers = this.props.customers.map(
+			customer => this.makeLink(customer)
+		);
 
-    return (
-      <div>
-        <h2>List of customers:</h2>
-        <List component="nav">{ customers }</List>
-      </div>
-    );
-  }
+		return (
+			<div>
+				<h2>List of customers:</h2>
+				<List component="nav">{ customers }</List>
+			</div>
+		);
+	}
 
-  makeLink(customer) {
-    return (
-      <ListItem key={customer._id}>
-        <ListItemText primary={customer.name} />
-      </ListItem>
-    );
-  }
+	makeLink(customer) {
+		return (
+			<ListItem key={customer._id}>
+				<ListItemText primary={customer.name} />
+			</ListItem>
+		);
+	}
 }
 
 export default InfoContainer = withTracker(() => {
-  return {
-    customers: Customers.find().fetch(),
-  };
+	return {
+		customers: Customers.find().fetch(),
+	};
 })(Info);
